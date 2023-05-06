3716b784f32a86ca8edf3f8e74887d5415cd35eb
AmirReza Farokhy <113052872+AmirRezaFarokhy@users.noreply.github.com>
2023-05-07 01:37:00 +0330

    Update

--- symbol_name/request_normal_range.py
+++ symbol_name/request_normal_range.py
@@ -4,10 +4,41 @@ import numpy as np
 
 urls_normal_range = "https://www.msdmanuals.com/professional/resources/normal-laboratory-values/blood-tests-normal-values"
 
-get_normal_range = requests.get(urls_normal_range)
-df = pd.read_html(get_normal_range.text)[0]
+def GetNormalValue():
+    for conv, si in zip(df["Conventional Units"], df['SI Units']):
+        
+        try:
+            x_max_conv = conv.split('<')
+            x_max_si = si.split('<')
+            if len(x_max_conv)==2 and len(x_max_si)==2:
+                df["Conventional Units"].iloc[cnt] = float(x_max_conv[1][:3].strip())
+                df['SI Units'].iloc[cnt] = float(x_max_si[1][:4].strip())
+        except Exception as e:
+            pass        
+        
+        try:
+            x_min_conv = conv.split('>')
+            x_min_si = si.split('>')
+            if len(x_min_conv)==2 and len(x_min_si)==2:
+                df["Conventional Units"].iloc[cnt] = float(x_min_conv[1][:3].strip())
+                df['SI Units'].iloc[cnt] = float(x_min_si[1][:3].strip())
+        except Exception as e:
+            pass  
 
-sym_df = pd.read_csv("symbols.csv")
+        try:
+            x_center_max_conv = conv.split('–')[1].split(' ')[0]
+            x_center_min_conv = conv.split('–')[0]
+            x_center_max_si = si.split('–')[1].split(' ')[0]
+            x_center_min_si = si.split('–')[0]
+            if len(conv.split('–'))==2 and len(si.split('–'))==2:
+                df["Conventional Units"].iloc[cnt] = str(x_center_min_conv)+'-'+str(x_center_max_conv)
+                df['SI Units'].iloc[cnt] = str(x_center_min_si)+'-'+str(x_center_max_si)
+        except Exception as e:
+            pass
+        
+        cnt += 1
+    
+    return df
 
 def UpdateSymbols():
     closing_len = len(sym_df) - 1
@@ -34,5 +65,11 @@ def UpdateSymbols():
 
     return sym_df
 
+get_normal_range = requests.get(urls_normal_range)
+df = pd.read_html(get_normal_range.text)[0]
+sym_df = pd.read_csv("symbols.csv")
+
+df = GetNormalValue()
 main_df = UpdateSymbols()
+
 main_df.to_csv("Update_symbols_with_normal_range.csv")
