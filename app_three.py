cce1646e3bb0c16b7bb20bbf8a278fbb56885341
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-10 16:17:37 +0330

    Adding New Features for Extract.py

---
+++ app_three.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import os 
+
+from Extract import PreprocessingTextFromImage
+
+img_path = 'img_test/img_2.jpg'
+CHUNK_W = 4
+CHUNK_H = 4
+
+App = PreprocessingTextFromImage(img_path, CHUNK_W, CHUNK_H)
+
+if App.CountingTestText()==1 and App.CountingTestText()>0:
+    information_image = App.OneTestText()
+else:
+    information_image = App.SeveralTestText()
+
+App.ShowBoxesDetected(information_image[2][1])
