b9cc3bdae23c1a8425ae2d058246d811c3f8a896
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-13 02:56:17 +0330

    add new values

--- app_three.py
+++ app_three.py
@@ -11,7 +11,7 @@ from Extract import PreprocessingTextFromImage
 if sys.platform[:3]=='win':
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-IMG_PATH = 'img_test/img_3.jpg'
+IMG_PATH = 'img_test/img_2.jpg'
 CHUNK_W = 4
 CHUNK_H = 4
 
@@ -19,12 +19,8 @@ App = PreprocessingTextFromImage(IMG_PATH, CHUNK_W, CHUNK_H)
 
 if App.CountingTestText()==1 and App.CountingTestText()>0:
     print("Start With One...")
-    #information_image = App.OneTestText(resolution=False)
     info = App.OneTestText()
     df = pd.DataFrame(columns=['Test', 'Result'])
-    # print(info['Test'], len(info['Test']))
-    # print('#############################')
-    # print(info['Result'], len(info['Result']))
     df['Test'] = info['Test']
     df['Result'] = info['Result']
     print(df)
