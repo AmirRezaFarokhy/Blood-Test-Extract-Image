fbb4c87b9a425f1df1bef95984b03dba636dc144
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-14 17:34:30 +0330

    edite our web application

--- Main/views.py
+++ Main/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import ImageBloodTest
+from ModelsApp import app_three
+
 
 def HomePage(request):
+    return render(request=request, 
+                  template_name='index.html')
+
+
+def AnalysisBloodTest(request):
     if request.method=='POST':
         image = request.FILES['upload']
-        data = ImageBloodTest(information="Hi" ,image=image)   
-        data.save()
+        image_data = ImageBloodTest(information="Hi", image=image)   
+        image_data.save()
+        
         return render(request=request,
-                        template_name='index.html',
-                        context={'result':'successfully uploaded'})
-
-
-    return render(request=request, 
-                    template_name='index.html', 
-                    )
+                      template_name='result.html',
+                      context={'result':'successfully uploaded'})
 
--- ModelsApp/app_three.py
+++ ModelsApp/app_three.py
@@ -5,7 +5,7 @@ import os
 import sys
 import cv2
 
-from Extract import PreprocessingTextFromImage
+from ModelsApp.Extract import PreprocessingTextFromImage
 
 
 if sys.platform[:3]=='win':
@@ -17,41 +17,42 @@ IMAEG_NAME = os.listdir(IMG_PATH)
 CHUNK_W = 4
 CHUNK_H = 4
 
-App = PreprocessingTextFromImage(IMG_PATH+IMAEG_NAME[0], CHUNK_W, CHUNK_H)
+if len(IMAEG_NAME)!=0:
+    App = PreprocessingTextFromImage(IMG_PATH+IMAEG_NAME[0], CHUNK_W, CHUNK_H)
 
-if App.CountingTestText()==1 and App.CountingTestText()>0:
-    print("Start With One...")
-    info, images = App.OneTestText()
-    img = App.ShowBoxesDetected(images[2])
-    df = pd.DataFrame(columns=['Test', 'Result'])
-    df['Test'] = info['Test']
-    df['Result'] = info['Result']
-    print(df)
-    plt.imshow(img)
-    plt.show()   
-else:
-    print("Start With Several...")
-    information_image = App.SeveralTestText()
-    App.ExtractingFeaturesSeveralTest(information_image)
-    append_vertically = []
-    append_all = []
-    for inf in information_image:
-        image_one = App.ShowBoxesDetected(inf[0])
-        image_tow = App.ShowBoxesDetected(inf[1])
-        append_vertically.append(cv2.hconcat([image_one, image_tow]))
-        if len(append_vertically)==len(information_image):
-            for img in append_vertically:
-                append_all.append(cv2.vconcat([img[0], img[1]]))
-    
-    plt.imshow(append_vertically[1])
-    plt.show()        
-    
+    if App.CountingTestText()==1 and App.CountingTestText()>0:
+        print("Start With One...")
+        info, images = App.OneTestText()
+        img = App.ShowBoxesDetected(images[2])
+        df = pd.DataFrame(columns=['Test', 'Result'])
+        df['Test'] = info['Test']
+        df['Result'] = info['Result']
+        print(df)
+        plt.imshow(img)
+        plt.show()   
+    else:
+        print("Start With Several...")
+        information_image = App.SeveralTestText()
+        App.ExtractingFeaturesSeveralTest(information_image)
+        append_vertically = []
+        append_all = []
+        for inf in information_image:
+            image_one = App.ShowBoxesDetected(inf[0])
+            image_tow = App.ShowBoxesDetected(inf[1])
+            append_vertically.append(cv2.hconcat([image_one, image_tow]))
+            if len(append_vertically)==len(information_image):
+                for img in append_vertically:
+                    append_all.append(cv2.vconcat([img[0], img[1]]))
+        
+        plt.imshow(append_vertically[1])
+        plt.show()        
+        
 
-# Removing Image To Avoiding extra files
-for image in IMAEG_NAME:
-    file_path = os.path.join(IMG_PATH ,image)
-    os.remove(file_path)
-    print("Images removed succesfully...")
+    # Removing Image To Avoiding extra files
+    for image in IMAEG_NAME:
+        file_path = os.path.join(IMG_PATH ,image)
+        os.remove(file_path)
+        print("Images removed succesfully...")
 
 
 
